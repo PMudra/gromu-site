7a40de88247c7abeb65e1e6382f359453b870406
Philipp Mudra <philipp.mudra@ise.de>
2019-10-24 16:58:43 +0200

    Normalize.css

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,10 +1,12 @@
 import Typography from "typography"
 
 const typography = new Typography({
-  baseFontSize: "18px",
+  baseFontSize: "20px",
   // baseLineHeight: 1.666,
   headerFontFamily: ["Hind", "sans-serif"],
   bodyFontFamily: ["Hind", "sans-serif"],
+  includeNormalize: false,
+  scaleRatio: 3
 })
 
 export default typography
