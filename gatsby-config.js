392268e8746d2a48991659be5d115f0459633962
Philipp Mudra <philipp.mudra@web.de>
2019-11-01 22:13:45 +0000

    App manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `GroMu Fanclub`,
+        short_name: `GroMu`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#d9e4de`,
+        theme_color: `#d9e4de`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gromu-01.png`,
       },
     },
     {
