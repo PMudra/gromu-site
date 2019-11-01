5bbae8e2efd67d1943d7e9baceb3418754d31e56
Philipp Mudra <philipp.mudra@web.de>
2019-11-01 15:45:00 +0000

    Start working on layout

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,11 +2,13 @@ import Typography from "typography"
 
 const typography = new Typography({
   baseFontSize: "20px",
-  // baseLineHeight: 1.666,
+  //baseLineHeight: 1.0,
+  //blockMarginBottom: 0.75,
   headerFontFamily: ["Hind", "sans-serif"],
   bodyFontFamily: ["Hind", "sans-serif"],
   includeNormalize: false,
-  scaleRatio: 3,
+  // scaleRatio: 0.5,
+  //headerColor: "#08100c"
 })
 
 export default typography
