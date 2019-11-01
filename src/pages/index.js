cc2228faef9b5d11518501ce04f5e807f2025744
Philipp Mudra <philipp.mudra@web.de>
2019-11-01 21:50:54 +0000

    Clean up and add Helmet to start page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,12 @@
 import React from "react"
 import classnames from "classnames"
 import Image from "../components/image"
+import SEO from "../components/seo"
 import styles from "./index.module.css"
 
 const IndexPage = () => (
-  // <Layout>
-  //   <SEO title="Home" />
-  //   <h1>Hi people</h1>
-  //   <p>Welcome to your new Gatsby site.</p>
-  //   <p>Now go build something great.</p>
-  //   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-  //     <Image />
-  //   </div>
-  //   <Link to="/page-2/">Go to page 2</Link>
-  // </Layout>
   <div className={styles.layout}>
+    <SEO title="Startseite" />
     <section className={styles.section}>
       <div className={classnames(styles.intro, styles.content)}>
         <div className={styles.logo}>
@@ -77,28 +69,6 @@ const IndexPage = () => (
         </p>
       </div>
     </section>
-    {/* <div
-      style={{
-        backgroundColor: "white",
-        margin: "0 auto",
-        maxWidth: 640,
-        border: "1px solid rgba(50, 50, 50, 0.14)",
-        borderRadius: "10px",
-        boxShadow: "2px 4px 6px 1px rgba(50, 50, 50, 0.14)",
-      }}
-    >
-      <div style={{ padding: "40px 20px 0 20px" }}>
-        
-        <h1 style={{ textAlign: "center" }}>GroMu Werder Bremen Fanclub</h1>
-      </div>
-      <div
-        style={{
-          borderTop: "1px solid rgba(50, 50, 50, 0.14)",
-          padding: "20px 20px",
-        }}
-      >
-      </div>
-    </div> */}
   </div>
 )
 
