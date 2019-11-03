e807482de53c9013466bc8c2b76bfb21a35729c4
Philipp Mudra <philipp.mudra@web.de>
2019-11-03 20:31:27 +0000

    First work on match overview

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,29 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const moment = require("moment-timezone")
+moment.locale("de")
+
+exports.onCreateNode = ({ node, actions: { createNodeField } }) => {
+  if (node.internal.type === "matches") {
+    createNodeField({
+      node,
+      name: "localTime",
+      value: moment(node.utcDate)
+        .tz("Europe/Berlin")
+        .format("LT"),
+    })
+    createNodeField({
+      node,
+      name: "localDate",
+      value: moment(node.utcDate)
+        .tz("Europe/Berlin")
+        .format("dd. D.M."),
+    })
+    createNodeField({
+      node,
+      name: "scheduled",
+      value: moment(node.utcDate).hour() !== 0,
+    })
+  }
+}
