80f6f05ff7a5bd5716b6871c8d76991260464979
Philipp Mudra <philipp.mudra@web.de>
2020-05-02 16:18:47 +0200

    Upgrade and disable matches for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,9 @@ module.exports = {
         icon: `src/images/gromu-logo.png`,
       },
     },
+    // Provide support for using the css-in-js library Emotion including server side rendering.
     `gatsby-plugin-emotion`,
+    // A Gatsby plugin to download and prefetch Google Fonts. Can increase performance as opposed to loading webfonts from Google’s external stylesheet.
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
@@ -42,25 +44,27 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    {
-      resolve: "gatsby-source-apiserver",
-      options: {
-        headers: {
-          "X-Auth-Token": process.env.FOOTBALL_DATA_AUTH_TOKEN,
-        },
-        entitiesArray: [
-          {
-            url: `https://api.football-data.org/v2/teams/12`,
-            name: `team`,
-          },
-          {
-            url: `http://api.football-data.org/v2/teams/12/matches`,
-            name: `matches`,
-            entityLevel: `matches`,
-          },
-        ],
-      },
-    },
+    
+    // {
+    //   resolve: "gatsby-source-apiserver",
+    //   options: {
+    //     headers: {
+    //       "X-Auth-Token": process.env.FOOTBALL_DATA_AUTH_TOKEN,
+    //     },
+    //     entitiesArray: [
+    //       {
+    //         url: `https://api.football-data.org/v2/teams/12`,
+    //         name: `team`,
+    //       },
+    //       {
+    //         url: `http://api.football-data.org/v2/teams/12/matches`,
+    //         name: `matches`,
+    //         entityLevel: `matches`,
+    //       },
+    //     ],
+    //   },
+    // },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
--- src/components/matches.js
+++ src/components/matches.js
@@ -1,79 +1,79 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import styles from "./matches.module.css"
+// import React from "react"
+// import { useStaticQuery, graphql } from "gatsby"
+// import styles from "./matches.module.css"
 
-const Matches = () => {
-  const data = useStaticQuery(graphql`
-    {
-      allMatches(sort: { fields: matchday }) {
-        edges {
-          node {
-            id
-            awayTeam {
-              name
-            }
-            homeTeam {
-              name
-            }
-            matchday
-            status
-            fields {
-              scheduled
-              localTime
-              localDate
-            }
-            score {
-              fullTime {
-                homeTeam
-                awayTeam
-              }
-              halfTime {
-                awayTeam
-                homeTeam
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
+// const Matches = () => {
+//   const data = useStaticQuery(graphql`
+//     {
+//       allMatches(sort: { fields: matchday }) {
+//         edges {
+//           node {
+//             id
+//             awayTeam {
+//               name
+//             }
+//             homeTeam {
+//               name
+//             }
+//             matchday
+//             status
+//             fields {
+//               scheduled
+//               localTime
+//               localDate
+//             }
+//             score {
+//               fullTime {
+//                 homeTeam
+//                 awayTeam
+//               }
+//               halfTime {
+//                 awayTeam
+//                 homeTeam
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  return (
-    <>
-      <h1>Spiele</h1>
-      <div className={styles.matches}>
-        {data.allMatches.edges.map(
-          ({
-            node: {
-              id,
-              homeTeam: { name: homeTeam },
-              awayTeam: { name: awayTeam },
-              score,
-              fields: { scheduled, localDate, localTime },
-            },
-          }) => (
-            <React.Fragment key={id}>
-              <span className={styles.team}>
-                {homeTeam}
-              </span>
-              <div className={styles.info}>
-                <span className={styles.small}>
-                  {localDate} {scheduled && localTime}
-                </span>
-                <span>
-                  {score.fullTime.homeTeam} : {score.fullTime.awayTeam}
-                </span>
-                <span className={styles.small}>
-                  ({score.halfTime.homeTeam} : {score.halfTime.awayTeam})
-                </span>
-              </div>
-              <span className={styles.team}>{awayTeam}</span>
-            </React.Fragment>
-          )
-        )}
-      </div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <h1>Spiele</h1>
+//       <div className={styles.matches}>
+//         {data.allMatches.edges.map(
+//           ({
+//             node: {
+//               id,
+//               homeTeam: { name: homeTeam },
+//               awayTeam: { name: awayTeam },
+//               score,
+//               fields: { scheduled, localDate, localTime },
+//             },
+//           }) => (
+//             <React.Fragment key={id}>
+//               <span className={styles.team}>
+//                 {homeTeam}
+//               </span>
+//               <div className={styles.info}>
+//                 <span className={styles.small}>
+//                   {localDate} {scheduled && localTime}
+//                 </span>
+//                 <span>
+//                   {score.fullTime.homeTeam} : {score.fullTime.awayTeam}
+//                 </span>
+//                 <span className={styles.small}>
+//                   ({score.halfTime.homeTeam} : {score.halfTime.awayTeam})
+//                 </span>
+//               </div>
+//               <span className={styles.team}>{awayTeam}</span>
+//             </React.Fragment>
+//           )
+//         )}
+//       </div>
+//     </>
+//   )
+// }
 
-export default Matches
+// export default Matches
